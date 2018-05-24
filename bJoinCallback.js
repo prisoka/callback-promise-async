b0356217ad07205d4753a2efb6cfbb0c764fee18
jbays <jjbaize@gmail.com>
2018-05-23 17:14:15 -0700

    pushing up bJoinCallback solutions

--- bJoinCallback.js
+++ bJoinCallback.js
@@ -1,9 +1,36 @@
 const fs = require('fs');
 
 const joiner = (cb) => {
-    // You cannot use fs.readFileSync here
-    // Use https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-    cb([]);
+  fs.readFile('users.json', (readUserErr, userString)=>{
+    const users = JSON.parse(userString);
+    fs.readFile('books.json', (readBookErr, bookString)=>{
+      const books = JSON.parse(bookString);
+      fs.readFile('reviews.json', (readReviewErr, reviewString)=>{
+        const reviews = JSON.parse(reviewString);
+        let output = [];
+        for ( let singleReview of reviews ) {
+          for ( let singleBook of books ) {
+            if ( singleBook.id !== singleReview.bookId ) {
+              continue;
+            }
+            for ( let singleUser of users ) {
+              if ( singleUser.id !== singleReview.userId) {
+                continue;
+              }
+              let joined = {
+                "name":   singleUser.firstName,
+                "book":   singleBook.title,
+                "rating": singleReview.stars,
+                "review": singleReview.text
+              }
+              output.push(joined);
+            }
+          }
+        }
+        cb(output);
+      })
+    })
+  })
 };
 
 module.exports = joiner;
